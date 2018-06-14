0739db1f6f9ea8489f44921b9aae4132cf162007
jiraphon <jiraphon@sirimedia.com>
2018-06-14 18:51:12 +0700

    Lazy loading

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,12 @@ const autoprefixer = require('autoprefixer');
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
-  entry: './src/index',
+  entry: './src/index.js',
   output: {
-    path: '',
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: path.resolve(__dirname, 'dist')
+    chunkFilename: '[id].js',
+    publicPath: ''
   },
   resolve: {
     extensions: ['.js', '.jsx']
